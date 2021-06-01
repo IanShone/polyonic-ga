9af70a91d0c8b53e386db617da69ea8113407e23
Ian Shone <isho5991@gmail.com>
2021-06-01 15:11:15 +0100

    feat(app): basic event handle+logging

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,17 @@
-export class Analytics {
+// @ts-nocheck
+
+
+export class PolyonicGoogleAnalytics {
+
+  gtag: any
+  logging: boolean
+
+  packageName: string
+
+  constructor() {
+    this.packageName = '👓[Polyonic Google Analytics]:'
+    this.logging = true
+  }
 
   Greeter(name: string) {
     console.log(`Analytics says: Hello ${name}`)
@@ -6,29 +19,70 @@ export class Analytics {
   }
 
   InitialiseGA(GA_Measurement_ID: string) {
+    return new Promise((res, rej) => {
 
-    GA_Measurement_ID = 'G-8YDB3KTM4S'
+      let head = document.getElementsByTagName('head')[0]
 
-    let head = document.getElementsByTagName('head')[0]
+      let script1 = document.createElement('script')
+      script1.type = 'text/javascript'
+      script1.async = true
+      script1.src = `https://www.googletagmanager.com/gtag/js?id=${GA_Measurement_ID}`
+      head.appendChild(script1)
 
-    let script1 = document.createElement('script')
-    script1.type = 'text/javascript'
-    script1.src = `https://www.googletagmanager.com/gtag/js?id=${GA_Measurement_ID}`
-    head.appendChild(script1)
+      let script2 = document.createElement('script')
+      script2.type = 'text/javascript'
+      script2.async = true
+      script2.onload = this.gaInit(GA_Measurement_ID)
+      head.appendChild(script2)
 
-    let script2 = document.createElement('script')
-    script2.type = 'text/javascript'
-    script2.innerHTML = `window.dataLayer = window.dataLayer || []; function gtag() { dataLayer.push(arguments); }gtag('js', new Date()); gtag('config', ${GA_Measurement_ID});  gtag('event', 'page_view', { 'send_to': GA_Measurement_ID }) `
-    head.appendChild(script2)
+      Promise.resolve()
+        .then(() => res(null))
+        .catch(err => rej(err))
 
-    // ga('send', 'event', 'Startup')
+    })
+  }
 
-    console.log('Report sent')
+  // sendEvent(name: any, params: any) {
+  //   // const gclidPromise = new Promise(resolve => {
+  //   //   gtag('get', 'DC-XXXXXXXX', 'gclid', resolve)
+  //   console.log('Attempt')
+  //   this.gtag('event', name, params)
+  //   // })
+  //   //   .then()
+  // }
 
+  enableLogging(val) {
+    if (val) {
+      this.logging = true
+      console.log(`${this.packageName} Logging Enabled`)
+    } else {
+      this.logging = false
+      console.log(`${this.packageName} Logging Disabled`)
+    }
+  }
+
+  sendEvent(name: any, params: any) {
+    const ctx = this
+
+    this.gtag('event', name, {
+      'event_callback': function () { ctx.handleLogs(`Event "${name}" recorded.`) }
+    })
+  }
+
+  handleLogs(log) {
+    if (log) { console.log(`${this.packageName} ${log}`) }
+  }
+
+  gaInit(ID) {
+    window.dataLayer = window.dataLayer || []
+    this.gtag = function () { dataLayer.push(arguments) }
+    // function gtag() { dataLayer.push(arguments) }
+    this.gtag('js', new Date());
+    this.gtag('config', ID)
+    this.gtag('event', 'page_view', { 'send_to': ID })
   }
 
 }
 
 
-
-module.exports = { Analytics }
+module.exports = { PolyonicGoogleAnalytics }
